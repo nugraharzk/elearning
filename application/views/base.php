ad9ecacdc1b58a6ae394cd3ff3552e78d4936a80
Rizki Nugraha <nugraharizki80@gmail.com>
2020-03-01 11:16:57 +0700

    Added adminlte, ion auth, and bootstrap

---
+++ application/views/base.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php $header ?>
+</head>
+<body>
+    
+    <?php $content ?>
+
+    <?php $footer ?>
+
+</body>
+</html>
---
+++ application/views/partials/header.php
@@ -0,0 +1,16 @@
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>E-Learning | <?= $page ?></title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/ionicons.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/adminlte.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="<?= base_url() ?>assets/css/sanspro.css" rel="stylesheet">
